71a207e25b256fa14e298e31574436057d830267
litieying <1399848107@qq.com>
2022-01-02 23:34:20 +0800

    暂存详情页面

--- pagesA/cartoonDetails/index.vue
+++ pagesA/cartoonDetails/index.vue
@@ -1,5 +1,10 @@
 <template>
   <view>
+    <u-navbar :autoBack="true" fixed :placeholder="false" bgColor="rgba(255,255,255c,0)"
+      ><view class="u-nav-slot" slot="left">
+        <u-icon name="arrow-left" size="19"></u-icon>
+      </view>
+    </u-navbar>
     <view class="head">
       <u--image :src="detailInfo.avatar" width="100%" mode="aspectFill"></u--image>
     </view>
@@ -8,12 +13,16 @@
         {{ detailInfo.name }}
       </view>
       <view class="notability">
-        <text style="background: #c7c6c6"> 阅读数：22万 </text>
+        <text> <text style="color: #9aa089">阅读数</text> 22万 </text>
       </view>
       <view class="start">
         {{ detailInfo.caricature_status == "0" ? "连载中" : "已完结" }}
       </view>
-      <view class="other"></view>
+      <view class="other">
+        <text>{{ detailInfo.author }}</text>
+        <text> | </text>
+        <text>热血</text>
+      </view>
     </view>
     <view class="com"> </view>
     <!--目录-->
@@ -64,23 +73,41 @@ export default {
   computed: {},
 };
 </script>
-<style lang="less" scoped>
+<style lang="scss" scoped>
+page {
+  background: $u-bg-color;
+}
 .head {
   width: 100%;
   height: 300rpx;
   overflow: hidden;
 }
 .info {
-  padding: 20rpx;
+  padding: 35rpx;
+  view {
+    margin-bottom: 20rpx;
+  }
   .name {
-    font-size: 34rpx;
+    font-size: 40rpx;
     font-weight: bold;
+    letter-spacing: 5rpx;
   }
   .notability {
     font-size: 24rpx;
+    text {
+      background: #f2f2f2;
+      padding: 5rpx;
+    }
   }
   .start {
     font-size: 26rpx;
+  }
+  .other {
+    font-size: 24rpx;
+    text {
+      padding: 5rpx;
+      color: #9aa089;
+    }
   }
 }
 </style>
