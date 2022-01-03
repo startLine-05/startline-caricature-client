7ec67dd06caa89c4d8d917eb8c6eb69e5f7db5ec
litieying <1399848107@qq.com>
2022-01-03 23:07:45 +0800

    添加漫画详情页面

--- pagesA/cartoonDetails/index.vue
+++ pagesA/cartoonDetails/index.vue
@@ -1,6 +1,6 @@
 <template>
   <view>
-    <u-navbar :autoBack="true" fixed :placeholder="false" bgColor="rgba(255,255,255c,0)"
+    <u-navbar :autoBack="true" fixed :placeholder="false" bgColor="rgba(255,255,255,0)"
       ><view class="u-nav-slot" slot="left">
         <u-icon name="arrow-left" size="19"></u-icon>
       </view>
@@ -9,6 +9,9 @@
       <u--image :src="detailInfo.avatar" width="100%" mode="aspectFill"></u--image>
     </view>
     <view class="info">
+      <view class="store u-f-ajc">
+        <u-icon size="40rpx" name="star-fill" color="#fff" label="追漫" labelPos="bottom" labelSize="24rpx" labelColor="#fff" space="3rpx"></u-icon>
+      </view>
       <view class="name">
         {{ detailInfo.name }}
       </view>
@@ -16,18 +19,49 @@
         <text> <text style="color: #9aa089">阅读数</text> 22万 </text>
       </view>
       <view class="start">
-        {{ detailInfo.caricature_status == "0" ? "连载中" : "已完结" }}
+        <text>{{ detailInfo.caricature_status == "0" ? "连载中" : "已完结" }}</text>
+        <text> | </text>
+        <text> 已更新至67话 </text>
       </view>
       <view class="other">
-        <text>{{ detailInfo.author }}</text>
+        <text>作者：{{ detailInfo.author }}</text>
         <text> | </text>
         <text>热血</text>
       </view>
     </view>
     <view class="com"> </view>
+    <view class="tabbar u-f-ac">
+      <view>
+        <u-icon
+          size="60rpx"
+          name="file-text"
+          color="#9aa089"
+          label="目录"
+          labelPos="bottom"
+          labelSize="24rpx"
+          labelColor="#9aa089"
+          space="5rpx"
+          @click="showList = true"
+        ></u-icon>
+      </view>
+      <view></view>
+    </view>
     <!--目录-->
     <u-popup :show="showList" @close="showList = false">
-      <view> </view>
+      <view class="popup-title"> 全部章节 ({{ caricatureContentLength }}) </view>
+      <view class="popup-content">
+        <u-list>
+          <u-list-item v-for="(item, index) in detailInfo.caricatureContentList" :key="index">
+            <view class="chapter u-f">
+              <u--image :src="detailInfo.avatar" width="200rpx" height="112rpx" radius="10rpx" mode="aspectFill"></u--image>
+              <view class="chapter-info">
+                <view>{{ item.current_number }} {{ item.current_name }}</view>
+                <view>{{ item.create_date }}</view>
+              </view>
+            </view>
+          </u-list-item>
+        </u-list>
+      </view>
     </u-popup>
   </view>
 </template>
@@ -38,7 +72,7 @@ export default {
     // 页面数据变量
     return {
       detailInfo: {},
-      showList: false,
+      showList: true,
     };
   },
   // 监听 - 页面每次【加载时】执行(如：前进)
@@ -70,12 +104,17 @@ export default {
   // 过滤器
   filters: {},
   // 计算属性
-  computed: {},
+  computed: {
+    caricatureContentLength() {
+      return this.detailInfo.caricatureContentList?.length;
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
 page {
   background: $u-bg-color;
+  font-size: 24rpx;
 }
 .head {
   width: 100%;
@@ -84,8 +123,21 @@ page {
 }
 .info {
   padding: 35rpx;
+  position: relative;
   view {
     margin-bottom: 20rpx;
+  }
+  text {
+    padding: 5rpx;
+  }
+  .store {
+    width: 110rpx;
+    height: 90rpx;
+    background: #f9ae3d;
+    position: absolute;
+    top: -50rpx;
+    right: 40rpx;
+    border-radius: 20rpx 0;
   }
   .name {
     font-size: 40rpx;
@@ -93,20 +145,48 @@ page {
     letter-spacing: 5rpx;
   }
   .notability {
-    font-size: 24rpx;
     text {
-      background: #f2f2f2;
-      padding: 5rpx;
+      background: #e7e6e4;
     }
   }
   .start {
     font-size: 26rpx;
   }
   .other {
-    font-size: 24rpx;
     text {
-      padding: 5rpx;
       color: #9aa089;
+    }
+  }
+}
+.tabbar {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  height: 120rpx;
+  box-shadow: 5rpx 5rpx 10rpx #616161;
+  background: #fff;
+  view {
+    padding: 10rpx;
+  }
+}
+.popup-title {
+  height: 100rpx;
+  padding: 10rpx;
+  font-size: 35rpx;
+  font-weight: 510;
+}
+.popup-content {
+  height: 500rpx;
+  .chapter {
+    padding: 20rpx 30rpx;
+    .chapter-info {
+      font-size: 30rpx;
+      padding: 0 20rpx;
+      color: #767a82;
+      display: flex;
+      justify-content: space-between;
+      flex-direction: column;
     }
   }
 }
