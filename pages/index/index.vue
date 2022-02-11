23fb2fe1fd1e1b23c92205b93dae0f633971c6b9
litieying <1399848107@qq.com>
2022-02-11 17:44:21 +0800

    修改首页效果

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,9 +2,19 @@
   <view>
     <view class="head">
       <view class="search">
-        <u-search class="search" bgColor="#fff" :show-action="true" actionText="搜索" :animation="true"></u-search>
+        <u-search
+          class="search"
+          bgColor="#fff"
+          :show-action="true"
+          actionText="搜索"
+          :animation="true"
+          v-model="searchvalue"
+          @search="getCaricatureList"
+        ></u-search>
       </view>
-
+      <u-sticky class="tabs" bgColor="#f2f2f2">
+        <u-tabs :list="categoryList" keyName="label" @change="change"></u-tabs>
+      </u-sticky>
       <u-swiper
         :list="stickyList"
         keyName="avatar"
@@ -29,6 +39,10 @@
 
 <script>
 var pageIndex = 1;
+var tabs = {
+  label: "推荐",
+  value: "",
+};
 import card from "@/components/card/index";
 export default {
   components: {
@@ -37,35 +51,55 @@ export default {
   data() {
     // 页面数据变量
     return {
-      list: [],
-      stickyList: [],
+      isEmpty: false,
+      list: [], //漫画列表
+      stickyList: [], //置顶漫画列表
+      categoryList: [
+        {
+          label: "推荐",
+          value: "",
+        },
+      ], //分类
+      searchvalue: "",
     };
   },
   // 监听 - 页面每次【加载时】执行(如：前进)
-  onLoad(options = {}) {
-    this.init(options);
+  onLoad() {
+    this.getCaricatureList();
     this.getCategory();
     this.getCaricatureSticky();
   },
   onPullDownRefresh() {
     pageIndex = 1;
-    this.init();
+    this.reset();
+    this.getCaricatureList();
   },
+
   methods: {
     // 页面数据初始化函数
-    init(options) {
-      // promise方式
+    getCaricatureList() {
+      const { searchvalue, isEmpty } = this;
+      if (isEmpty) {
+        return;
+      }
       uni.vk
         .callFunction({
           url: "client/caricature/pub/getCaricatureList",
           title: "获取漫画列表...",
           data: {
-            pageIndex: 1,
+            pageIndex,
             pageSize: 10,
+            searchvalue,
+            category_id: tabs.value,
+            is_essence: tabs.value ? false : true,
           },
         })
         .then((res) => {
-          this.list = res.rows;
+          if (res.rows.length < 10) {
+            this.isEmpty = true;
+          }
+          pageIndex == 1 ? (this.list = res.rows) : this.list.push(...res.rows);
+          pageIndex++;
         })
         .finally(() => {
           uni.stopPullDownRefresh();
@@ -84,6 +118,7 @@ export default {
         })
         .then((res) => {
           uni.vk.setVuex("$cartoon.categoryList", res.data);
+          this.categoryList.push(...res.data);
         });
     },
     //获取置顶漫画
@@ -107,6 +142,17 @@ export default {
       const id = stickyList[index]._id;
       uni.vk.navigateTo(`/pagesA/cartoonDetails/index?id=${id}`);
     },
+    //重置
+    reset() {
+      pageIndex = 1;
+      this.isEmpty = false;
+    },
+    change(data) {
+      this.reset();
+      tabs = data;
+      console.log(data);
+      this.getCaricatureList();
+    },
   },
   // 过滤器
   filters: {},
@@ -119,8 +165,10 @@ export default {
   margin-bottom: 20rpx;
   background: #f2f2f2;
   .search {
+    padding: 10rpx 10rpx 0rpx 10rpx;
+  }
+  .tabs {
     padding: 10rpx;
-    margin-bottom: 40rpx;
   }
 }
 .list {
